b9ab14ab437dddbebbd67cea89ffe6f31cd4d389
Keshav Rajput <keshavraj9954@gmail.com>
2025-09-22 17:51:09 +0530

    Resolved Creatiion error

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { dummyCreationData } from '../assets/assets'
+// import { dummyCreationData } from '../assets/assets'
 import { Gem, Sparkles } from 'lucide-react'
 import { Protect, useAuth } from '@clerk/clerk-react'
-import CreationItem from '../components/CreationItem'
+// import CreationItem from '../components/CreationItem'
+import CreationItem from '../components/Creationitem'
 import axios from 'axios'
 import toast from 'react-hot-toast'
 
